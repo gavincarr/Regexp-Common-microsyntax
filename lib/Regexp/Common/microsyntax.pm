0d1596edf1d081d3048a92d17544aa68e55b035c
Gavin Carr <gavin@openfusion.com.au>
2011-07-19 17:19:33 +1000

    Initial import.

---
+++ lib/Regexp/Common/microsyntax.pm
@@ -0,0 +1,110 @@
+package Regexp::Common::microsyntax;
+
+use warnings;
+use strict;
+
+=head1 NAME
+
+Regexp::Common::microsyntax - The great new Regexp::Common::microsyntax!
+
+=head1 VERSION
+
+Version 0.01
+
+=cut
+
+our $VERSION = '0.01';
+
+
+=head1 SYNOPSIS
+
+Quick summary of what the module does.
+
+Perhaps a little code snippet.
+
+    use Regexp::Common::microsyntax;
+
+    my $foo = Regexp::Common::microsyntax->new();
+    ...
+
+=head1 EXPORT
+
+A list of functions that can be exported.  You can delete this section
+if you don't export anything, such as for a purely object-oriented module.
+
+=head1 SUBROUTINES/METHODS
+
+=head2 function1
+
+=cut
+
+sub function1 {
+}
+
+=head2 function2
+
+=cut
+
+sub function2 {
+}
+
+=head1 AUTHOR
+
+Gavin Carr, C<< <gavin at openfusion.com.au> >>
+
+=head1 BUGS
+
+Please report any bugs or feature requests to C<bug-regexp-common-microsyntax at rt.cpan.org>, or through
+the web interface at L<http://rt.cpan.org/NoAuth/ReportBug.html?Queue=Regexp-Common-microsyntax>.  I will be notified, and then you'll
+automatically be notified of progress on your bug as I make changes.
+
+
+
+
+=head1 SUPPORT
+
+You can find documentation for this module with the perldoc command.
+
+    perldoc Regexp::Common::microsyntax
+
+
+You can also look for information at:
+
+=over 4
+
+=item * RT: CPAN's request tracker
+
+L<http://rt.cpan.org/NoAuth/Bugs.html?Dist=Regexp-Common-microsyntax>
+
+=item * AnnoCPAN: Annotated CPAN documentation
+
+L<http://annocpan.org/dist/Regexp-Common-microsyntax>
+
+=item * CPAN Ratings
+
+L<http://cpanratings.perl.org/d/Regexp-Common-microsyntax>
+
+=item * Search CPAN
+
+L<http://search.cpan.org/dist/Regexp-Common-microsyntax/>
+
+=back
+
+
+=head1 ACKNOWLEDGEMENTS
+
+
+=head1 LICENSE AND COPYRIGHT
+
+Copyright 2011 Gavin Carr.
+
+This program is free software; you can redistribute it and/or modify it
+under the terms of either: the GNU General Public License as published
+by the Free Software Foundation; or the Artistic License.
+
+See http://dev.perl.org/licenses/ for more information.
+
+
+=cut
+
+1; # End of Regexp::Common::microsyntax
